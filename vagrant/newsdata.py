830d570fbd536f5b80396f5190fbcf54e8a42787
Marina Lupin <lupinmarina@gmail.com>
2017-05-15 09:04:59 -0700

    add shebang line and format queries

--- vagrant/newsdata.py
+++ vagrant/newsdata.py
@@ -1,17 +1,27 @@
+#!/usr/bin/env python
+
 import psycopg2
 import datetime
 
-def connect():
+
+def connect(database_name="news"):
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=news")
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("<error message>")
 
 
 def executeQuery(query, *kw):
-    db = connect()
-    db_cursor = db.cursor()
-    db_cursor.execute(query, *kw)
+    db, cursor = connect()
+    cursor.execute(query, *kw)
+    results = [(str(row[0]), round((row[1]), 2))
+               for row in cursor.fetchall()]
     db.commit()
     db.close()
+    return results
 
 
 def question1():
@@ -20,14 +30,13 @@ def question1():
     Returns a list of three most popular articles of all time
     with number of views, sorted by views in desc order.
     """
-    db = connect()
-    db_cursor = db.cursor()
-    query = "SELECT title, views FROM article_views ORDER BY views DESC LIMIT 3"
-    db_cursor.execute(query)
-    results = [(str(row[0]), int(row[1]))
-               for row in db_cursor.fetchall()]
-    db.commit()
-    db.close()
+    query = """
+            SELECT title, views
+            FROM article_views
+            ORDER BY views DESC
+            LIMIT 3
+            """
+    results = executeQuery(query)
 
     for i in xrange(len(results)):
         print "%s - %d views" % (results[i][0], results[i][1])
@@ -39,14 +48,13 @@ def question2():
     Returns a list of most popular authors of all time
     with number of views, sorted by views in desc order.
     """
-    db = connect()
-    db_cursor = db.cursor()
-    query = "SELECT name, sum(views) AS views FROM author_views_by_article GROUP BY name ORDER BY views DESC"
-    db_cursor.execute(query)
-    results = [(str(row[0]), int(row[1]))
-               for row in db_cursor.fetchall()]
-    db.commit()
-    db.close()
+    query = """
+            SELECT name, sum(views) AS views
+            FROM author_views_by_article
+            GROUP BY name
+            ORDER BY views DESC
+            """
+    results = executeQuery(query)
 
     for i in xrange(len(results)):
         print "%s - %d views" % (results[i][0], results[i][1])
@@ -57,20 +65,21 @@ def question3():
     """
     Returns list of days with more than 1% of requests that lead to errors
     """
-    db = connect()
-    db_cursor = db.cursor()
-    query = "SELECT date, 100.0 * errors/requests as perc_errors FROM date_requests WHERE 100.0 * errors/requests > 1 ORDER BY perc_errors DESC"
-    db_cursor.execute(query)
-    results = [(str(row[0]), round((row[1]), 2))
-               for row in db_cursor.fetchall()]
-    db.commit()
-    db.close()
+    query = """
+            SELECT date,
+            100.0 * errors/requests as perc_errors
+            FROM date_requests
+            WHERE 100.0 * errors/requests > 1
+            ORDER BY perc_errors DESC
+            """
+    results = executeQuery(query)
 
     for i in xrange(len(results)):
         date = datetime.datetime.strptime(results[i][0],
                                           '%Y-%m-%d').strftime("%B %d, %Y")
         print "%s - %s%% errors" % (date, results[i][1])
     return results
+
 
 print "\nThree most popular articles of all time:"
 question1()
